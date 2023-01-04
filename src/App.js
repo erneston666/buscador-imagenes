b61d34d81235c515e3b6ea82a817502361989474
erneston666 <erneston666@gmail.com>
2023-01-04 01:25:23 -0500

    llamado a la api y mostrando

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
+import { useState } from "react";
 import { Formik, Form, Field } from "formik";
-import './header.css'
+import "./header.css";
 const App = () => {
+  const [photos, setPhotos] = useState([]);
+  const open = url => window.open(url)
+  console.log({ photos });
   return (
     <div>
       <header>
         <Formik
-        initialValues={{search:''}}
-        onSubmit= {async values=>{
-          //llamar api unsplash
-          console.log(values)
-        }}
+          initialValues={{ search: "" }}
+          onSubmit={async (values) => {
+            const response = await fetch(
+              `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+              {
+                headers: {
+                  Authorization:
+                    "Client-ID eCba8Dn4bWKuFgJiTaTqBhEv4CaMshlDFcdteiOrQDo",
+                },
+              }
+            );
+            const data = await response.json();
+            //llamar api unsplash
+            setPhotos(data.results);
+          }}
         >
           <Form>
             <Field name="search" />
           </Form>
-
         </Formik>
       </header>
+      <div className="container">
+        <div className="center">
+          {photos.map(photo =>
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.regular} alt='' />
+              <p>{[photo.description, photo.alt_description].join('-')}</p>
+              
+            </article>
+        )}
+
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
