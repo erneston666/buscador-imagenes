d1cdffa766fd1274309492b1d96b8400fc4cd0a2
erneston666 <erneston666@gmail.com>
2023-01-04 01:02:52 -0500

    header, css, form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Formik, Form, Field } from "formik";
+import './header.css'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+        initialValues={{search:''}}
+        onSubmit= {async values=>{
+          //llamar api unsplash
+          console.log(values)
+        }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name="search" />
+          </Form>
+
+        </Formik>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
